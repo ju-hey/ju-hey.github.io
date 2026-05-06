9bb6c92827a9962fabd27aa0276eb759de663e12
ju-hey <ju-hey@users.noreply.github.com>
2026-05-06 16:09:49 +0000

    deploy: ju-hey/blog-data@e5626dc882e401ab8dbf415c2d41b59c7e169e73

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "깃허브 블로그 시작하기",
+        "excerpt":"이 페이지는 원래 테스트용으로 작성해뒀던 포스트이지만, 첫 포스트이기 때문에 없애지 않고 내용만 바꿔 남기려고 한다. &nbsp; 이 고생길은 약 두 달 전, 주헤이의 객기로 시작되었다. &nbsp; &nbsp; &nbsp; 처음에 선택한 건 티스토리였다. 따라서 처음 시작하기에 과하진 않았다. &nbsp; 주헤이가 생각했던 건 단순히, &nbsp; (1) 블로그 만들기 - 시작이 반이므로.. (2)...","categories": ["memo"],
+        "tags": ["일상","테스트","깃허브","취미"],
+        "url": "/memo/my-first-post/",
+        "teaser": "https://ju-hey-console.vercel.app/api/images/my-first-post/08ad3b13-8539-484c-8d2b-6e3c34b10c0e.jpg"
+      },{
+        "title": "[남유럽2개국] 2026년 2월 비수기 자유여행 계획 (1) - 어디 갈지 결정하기",
+        "excerpt":"튀르키예 이스탄불 - 보스포러스 크루즈에서 내리면서 찍은 풍경. &nbsp; 기존에 터키였던 국가 명칭이 튀르키예로 바뀐 지 벌써 몇 년이 지났지만 아직 모르는 사람도 있더라, 는 주헤이네에도 있었다ㅎㅎ &nbsp; 명칭이 바뀐 시점에 코로나 팬데믹 중이었기 때문에 모르는 분들이 조금 있고, &nbsp; 아직까지 예전 이름이 익숙하다 보니 국제적으로도 혼용하는 경우가 더러 있다....","categories": ["travel"],
+        "tags": ["겨울 유럽여행","튀르키예 여행","터키 여행","비수기 유럽","비수기 유럽여행 후기","유럽여행","Türkiye","February Travel","GreeceInWinter","OffSeasonEurope"],
+        "url": "/travel/post-001/",
+        "teaser": "https://ju-hey-console.vercel.app/api/images/post-001/6cd756d3-38b4-4b05-a136-bbce6e4a1d94.jpg"
+      }]
